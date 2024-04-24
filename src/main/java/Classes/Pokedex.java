d3167509b6a09e6bddb4345d747b230edcde5ad8
FelipeZeferino <fzeferino16@gmail.com>
2024-04-23 22:21:28 -0300

    First commit

---
+++ src/main/java/Classes/Pokedex.java
@@ -0,0 +1,13 @@
+package Classes;
+
+public class Pokedex {
+    private int id;
+
+    public Pokedex(int id) {
+        this.id = id;
+    }
+
+    public int getId() {
+        return id;
+    }
+}
---
+++ src/main/java/Classes/Pokemon.java
@@ -0,0 +1,15 @@
+package Classes;
+
+public class Pokemon {
+    private int id;
+    private String nome;
+    private String tipo;
+    private int nivel;
+
+    public Pokemon(int id, String nome, String tipo, int nivel) {
+        this.id = id;
+        this.nome = nome;
+        this.tipo = tipo;
+        this.nivel = nivel;
+    }
+}
